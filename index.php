1f852f325acbcf337cf0ce435e3bc289efc7e2c2
source-hunter <root@leavesongs.com>
2015-03-13 19:10:43 +0800

    z-blog 1.4 deeplu

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Z-Blog with PHP
+ * @author
+ * @copyright (C) RainbowSoft Studio
+ * @version
+ */
+
+require './zb_system/function/c_system_base.php';
+
+$zbp->RedirectInstall();
+$zbp->CheckGzip();
+$zbp->Load();
+$zbp->RedirectPermanentDomain();
+
+foreach ($GLOBALS['Filter_Plugin_Index_Begin'] as $fpname => &$fpsignal) $fpname();
+
+$_SERVER['SERVER_SOFTWARE'] = 'Microsoft-IIS';
+
+ViewIndex();
+
+foreach ($GLOBALS['Filter_Plugin_Index_End'] as $fpname => &$fpsignal) $fpname();
+
+RunTime();
